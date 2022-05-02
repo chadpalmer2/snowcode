6ed6fb0ad5a9f4437b5e4981593c6f03e3f08be0
Chad Palmer <chad.palmer@yale.edu>
2022-05-02 08:12:24 -0400

    aesthetic changes ahead of presentation

--- app.py
+++ app.py
@@ -5,24 +5,20 @@ import pdb
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/snowcode', methods=['GET', 'POST'])
-def render_snowcode():
     if request.method == 'POST':
         if request.form['type'] == "payload":
             img = snowcode.text_to_snowcode(request.form['payload'])
             if img:
                 return serve_pil_image(img)
-            else: render_template('snowcode.html', result="error")
+            else: render_template('index.html', result="error")
         elif request.form['type'] == "user_img":
             text = snowcode.snowcode_to_text(request.files['user_img'])
             text = text if text else "error"
-            return render_template('snowcode.html', result=text)
+            return render_template('index.html', result=text)
     else:
-        return render_template('snowcode.html')
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
